9ad4946ab64c8db9c852ffc0cd1fa8fdc35f7714
Juan Di Modugno <dimodugnojuan@gmail.com>
2017-05-18 23:50:12 -0300

    added test for components

---
+++ app/tests/components/TodoList.test.jsx
@@ -0,0 +1,35 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var TestUtils = require('react-addons-test-utils');
+var expect = require('expect');
+var $ = require('jQuery');
+
+var TodoList = require('TodoList');
+var Todo = require('Todo');
+
+describe('TodoList', () => {
+  it('should exist', () => {
+    expect(TodoList).toExist();
+  });
+
+  it('should render one todo for each todo item', () => {
+    var todos = [{
+      id: 1,
+      text: 'Walk the dog'
+    }, {
+      id: 2,
+      text: 'Feed the cat'
+    }, {
+      id: 3,
+      text: 'Clean the yard'
+    }, {
+      id: 4,
+      text: 'Take a shower'
+    }];
+    
+    var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos}/>);
+    var todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, Todo);
+
+    expect(todosComponents.length).toBe(4);
+  });
+});
